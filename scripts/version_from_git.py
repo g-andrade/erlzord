fb5bdda4ce6684e70618b8d1d876bd13f0bab109
Guilherme Andrade <g@gandrade.net>
2017-04-30 18:37:45 +0100

    Improve version extraction

--- scripts/version_from_git.py
+++ scripts/version_from_git.py
@@ -9,6 +9,10 @@ head_parts = git_head.split('/')
 if len(head_parts) > 2:
     if head_parts[-2] in ['release', 'hotfix', 'support']:
         print head_parts[-1]
+    elif len(head_parts) == 3 and head_parts[-1] in ['master']:
+        tag_description_parts = tag_description.split('-')
+        (major, minor, fix) = parse.parse('{:d}.{:d}.{:d}', tag_description_parts[0])
+        print '%d.%d.%d' % (major, minor, fix)
     else:
         print tag_description
 else:
